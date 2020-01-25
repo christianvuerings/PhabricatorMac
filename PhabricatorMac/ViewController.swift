085792ce4247bfc022d8e05512f326846de0e4a4
Christian Vuerings <cvuerings@pinterest.com>
2020-01-25 11:34:40 -0800

    Initial Commit

--- PhabricatorMac/ViewController.swift
+++ PhabricatorMac/ViewController.swift
@@ -9,9 +9,15 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var output: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        output.value(forKey: "Hello World")
+        
+        output.stringValue = "  result.sum"
+        
 
         // Do any additional setup after loading the view.
     }
